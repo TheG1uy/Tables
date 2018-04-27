ca21d49e25820716015dcb86c123c9c2af91f1c5
TheG1uy <paulnvidia@gmail.com>
2018-04-27 18:04:15 +0300

    Add TSortTable

---
+++ Tables/include/TSortTable.h
@@ -0,0 +1,81 @@
+#pragma once
+#pragma once
+#include "TArrayTable.h"
+
+
+template <class TKey, class TValue>
+class TSortTable : public TArrayTable<TKey, TValue> {
+	virtual bool Find(TKey _key) {
+		int left = 0, right = dataCount, middle;		
+		while(left <= right){
+			Eff++;
+			middle = (left + right) / 2;
+
+			if (arr[middle].key == _key) {
+				curr = middle;
+				return true;
+			}
+
+			if (arr[middle].key < _key) left = middle + 1;
+			else right = middle - 1;
+		}
+		curr = left;
+		return false;
+	}
+	virtual void Insert(TRecord<TKey, TValue> record) {
+
+		if (!Find(record.key)) {
+			if (isFull()) return;
+			for (int i = dataCount; i > curr; i--) {
+				arr[i] = arr[i - 1];
+				Eff++;
+			}
+			arr[curr] = record;
+			dataCount++;
+		}
+		else {
+
+		}
+	}
+	virtual void Delete(TKey _key) {
+		if (Find(_key)) {
+			for (int i = curr; i < dataCount - 1; i++) {
+				arr[i] = arr[i + 1];
+				Eff++;
+			}
+			dataCount--;
+		}
+	}
+	void QuickSort(int left, int right) {
+		int middle = (left + right) / 2;
+		TKey x = arr[middle].key;
+		TRecord<TKey, TValue> tmp;
+		int i = left, j = right;
+
+		while (i <= j) {
+			while (arr[i].key < x) {
+				i++;
+				Eff++;
+			}
+			while (arr[j].key > x) {
+				j--;
+				Eff++;
+			}
+
+			if (i <= j) {
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+				i++;
+				j--;
+			}
+
+			if (j > left) {
+				QuickSort(left, j);
+			}
+			if (i < right) {
+				QuickSort(i, right);
+			}
+		}
+	}
+};
